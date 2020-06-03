a4d69a893624349b8800f76739c967a259762911
leofan <xingyefan1990@gmail.com>
2020-06-03 10:01:32 -0400

    change folder

--- EntityRecognition.py
+++ EntityRecognition.py
@@ -1,7 +1,21 @@
 import json
+import spacy
+from spacy import displacy
 
 
-with open("train-v2.0.json") as f:
+nlp = spacy.load("en_core_web_lg")
+with open("/Users/leo/PycharmProjects/EECS4080/train-v2.0.json") as f:
     data = json.load(f)
-for i in range(20):
-    print(data["data"][i]["paragraphs"])
+count = 0
+result = []
+for i in range(2):
+    for d in data["data"][i]["paragraphs"]:
+        if count == 20:
+            break
+        if "context" in d.keys():
+            print(d["context"])
+            result.append(d["context"])
+            print("\n---------------------------------------------------------------------------\n")
+            count += 1
+
+print(count)
